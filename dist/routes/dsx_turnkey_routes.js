6308fdfab8179cec68bf456fe67be3d4f06f0286
Miljan9602 <rakitamiljan@yahoo.com>
2024-10-30 12:34:29 +0100

    Changed method

--- dist/routes/dsx_turnkey_routes.js
+++ dist/routes/dsx_turnkey_routes.js
@@ -8,4 +8,4 @@ const dsx_turnkey_controller_1 = require("../controllers/dsx_turnkey_controller"
 const express_1 = __importDefault(require("express"));
 const dsx_turnkey_router = express_1.default.Router();
 exports.dsx_turnkey_router = dsx_turnkey_router;
-dsx_turnkey_router.get("/suborg", dsx_turnkey_controller_1.dsx_turnkey_controller);
+dsx_turnkey_router.post("/suborg", dsx_turnkey_controller_1.dsx_turnkey_controller);
--- src/routes/dsx_turnkey_routes.ts
+++ src/routes/dsx_turnkey_routes.ts
@@ -4,6 +4,6 @@ import express from "express";
 
 const dsx_turnkey_router = express.Router();
 
-dsx_turnkey_router.get("/suborg", dsx_turnkey_controller);
+dsx_turnkey_router.post("/suborg", dsx_turnkey_controller);
 
 export {dsx_turnkey_router};
